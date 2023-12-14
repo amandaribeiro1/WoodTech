d786c69959e0b117f7938217589ca77d68eaeebf
amandaribeiro1 <amanda.costa@sptech.school>
2023-12-14 09:56:24 -0300

    Att para apresentação

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "woodtech",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "rootroot",
 };
 
 function executar(instrucao) {
